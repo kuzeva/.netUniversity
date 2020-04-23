c29682afaf74d392892d9b1b82cfe0bbad284017
Bojana Kuzeva <kuzeva01@gmail.com>
2020-04-23 16:21:10 +0200

    SeddData class added

--- UniversityStudentsApp/UniversityStudentsApp/Program.cs
+++ UniversityStudentsApp/UniversityStudentsApp/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using UniversityStudentsApp.Data;
+using UniversityStudentsApp.Models;
 
 namespace UniversityStudentsApp
 {
@@ -13,10 +15,27 @@ namespace UniversityStudentsApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<UniversityStudentsAppContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
